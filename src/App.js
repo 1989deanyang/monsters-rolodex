cd2ab1e1d0f447253eff3289ed41b85dd9e86029
longfei yang <longfei.yang@veovo.com>
2021-05-09 14:59:11 +1200

    create search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Component } from "react";
 import { CardList } from "./components/card-list/card-list.componets";
 import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
             monsters: [],
+            searchField: "",
         };
     }
 
@@ -17,10 +20,23 @@ class App extends Component {
         });
     }
 
+    // we use the arrow function to automatically assign the scope of this keyword in the function
+    // otherwise, we need to use "this.handlexxx = this.hanlexxx.bind(this)" in the constructor to make sure this keyword is correctly refereced in this function
+    handleSearchInputChange = (e) => {
+        this.setState({
+            searchField: e.target.value,
+        });
+    }
+
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters}></CardList>
+                <SearchBox placeholder="search monsters" handleChange={this.handleSearchInputChange} />
+                <CardList monsters={filteredMonsters}></CardList>
             </div>
         );
     }
