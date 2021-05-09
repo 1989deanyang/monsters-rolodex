10dcea295f9a0233d281bd62bb19ff2ad597681e
longfei yang <longfei.yang@veovo.com>
2021-05-09 13:44:36 +1200

    card component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
-import { Component } from 'react';
-import { CardList } from './components/card-list/card-list.componets';
-import './App.css';
+import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.componets";
+import "./App.css";
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      monsters: [
-        {
-          id: "er",
-          name: "Frankestin"
-        },
-        {
-          id: "er2",
-          name: "Dracula"
-        },
-        {
-          id: "er3",
-          name: "Zombie"
-        }
-      ]
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+        };
     }
-  }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(response => {
-      response?.json().then(data => {
-        this.setState({ monsters: data })
-      });
-    })
-  }
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
+            response?.json().then((data) => {
+                this.setState({ monsters: data });
+            });
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <CardList>
-          {
-            this.state.monsters.map(monster => {
-              return (
-                <h1 key={monster.id}>{monster.name}</h1>
-              )
-            })
-          }
-        </CardList>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="App">
+                <CardList monsters={this.state.monsters}></CardList>
+            </div>
+        );
+    }
 }
 
 export default App;
